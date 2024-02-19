1db95c690352dbb521833f2bb2de3046423a245c
rocoriyan <rcorrigan.other+github@gmail.com>
2024-02-19 11:23:41 +0000

    chore: setup

---
+++ src/users/controllers.js
@@ -0,0 +1,4 @@
+const User = require("./model");
+
+module.exports = {
+};
---
+++ src/users/routes.js
@@ -0,0 +1,6 @@
+const { Router } = require("express");
+const userRouter = Router();
+
+const cntrlFuncs = require("./controllers"); //controller functions
+
+module.exports = userRouter;
